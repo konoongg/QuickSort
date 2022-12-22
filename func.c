13e974d8c96cd7683bdfdb3c4674b791181e59a2
konoongg <120199924+konoongg@users.noreply.github.com>
2022-12-22 20:25:15 +0700

    Fix

--- func.c
+++ func.c
@@ -3,39 +3,35 @@
 #include "func.h"
 
 void Swap(int* first, int* second) {
-	int temp = *first;
-	*first = *second;
-	*second = temp;
+    int temp = *first;
+    *first = *second;
+    *second = temp;
 }
 
-void QuickSort(int* numbers, size_t start, size_t end, size_t size) {
-	if (end - start < 1) {
-		return;
-	}
-	size_t left = start;
-	size_t right = end;
-	size_t pvot = (start + rand()%(end +1 - start ));
-	while (left < right) {
-		while (numbers[left] < numbers[pvot] ) {
-			++left;
-		}
-		while (numbers[right] > numbers[pvot] ) {
-			--right;
-		}
-		if (left < right) {
-			Swap(&numbers[left],&numbers[right]);
-			//for (int i = left; i <= right; ++i) {
-				//printf("%d ", numbers[i]);
-			//}
-			//printf("\n");
-			++left;
-			--right;
-		}
-		
+void QuickSort(int* numbers, size_t start, size_t end) {
+    if (end - start < 1) {
+        return;
     }
-	if (end - start > 1) {
-		QuickSort(numbers, left, end, size);
-		QuickSort(numbers, start, right, size);
-	}
-	
+    size_t left = start;
+    size_t right = end;
+    size_t pvot = (start + rand() % (end - start +1));
+    while (left < right) {
+        while (numbers[left] < numbers[pvot] ) {
+            ++left;
+        }
+        while (numbers[right] > numbers[pvot] ) {
+            --right;
+        }
+        if (left < right) {
+            Swap(&numbers[left],&numbers[right]);
+            --right;
+            ++left;
+        }
+        
+    }
+    if (end - start > 1) {
+        QuickSort(numbers, left, end);
+        QuickSort(numbers, start, right);
+    }
+    
 }
